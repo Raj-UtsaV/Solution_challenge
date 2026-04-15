69b5859a2bac073fe0019dd189168a0bccfb0c5d
Raj_Utsav <utsavraj911@outlook.com>
2026-04-15 22:05:57 +0530

    final changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, make_response
 from src.preprocess import PREPROCESSING
 from src.predictor import PREDICTOR
 from src.llm_engine import CustomerData, CreditLLMEngine
+import os
 
 
 app = Flask(__name__)
@@ -71,6 +72,5 @@ def predict():
         }), 500
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
