60812b1d0dfe8cb744414dcc2e19a0e1a38c934d
Raj_Utsav <utsavraj911@outlook.com>
2026-04-15 11:06:12 +0530

    Improved UI

--- app.py
+++ app.py
@@ -1,48 +1,68 @@
 from flask import Flask, render_template, request, jsonify
-import joblib
 from src.preprocess import PREPROCESSING
 from src.predictor import PREDICTOR
-from src.llm_engine import CustomerData,CreditLLMEngine
-import pandas as pd
-LLM_ENGINE = CreditLLMEngine()
+from src.llm_engine import CustomerData, CreditLLMEngine
+
 
 app = Flask(__name__)
+LLM_ENGINE = CreditLLMEngine()
 
 
 @app.route('/')
-def index():
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict-page')
+def predict_page():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # 1. Get data from the frontend (this is a dictionary)
-        user_input = request.json
-        
-        # 2. ML Preprocessing & Prediction
+
+        user_input = request.get_json()
+        print(user_input)
+        if not user_input:
+            return jsonify({
+                'status': 'error',
+                'message': 'No input data provided'
+            }), 400
+
+
         processor = PREPROCESSING(user_input)
         processed_data = processor.run()
+
+
         predictor = PREDICTOR(processed_data)
-        ml_prediction = predictor.run()[0] 
+        ml_prediction = predictor.run()[0]  
+        ml_output: "High Risk" if ml_prediction == "bad" else "Low Risk"
 
-        # 4. Prepare LLM Input (Merge the dicts)
-        # This creates a single dictionary containing all user_input PLUS the Risk
-        llm_payload = {**user_input, "Risk": ml_prediction}
+        llm_payload = {
+            **user_input,
+            "Risk": ml_output
+        }
 
-        # 5. Get LLM Analysis
-        # Pydantic will now receive a clean dictionary with 'good' or 'bad' for Risk
         customer = CustomerData(**llm_payload)
         llm_text = LLM_ENGINE.get_description(customer)
-        
-        return jsonify({
-            'ml_output': "High Risk" if ml_prediction=="bad" else "Low Risk",
-            'llm_output': llm_text,
-            'status': 'success'
-        })
-        
-    except Exception as e:
-        print(f"Log Error: {e}") # This helps you see the trace in your terminal
-        return jsonify({'status': 'error', 'message': str(e)})
 
+        return jsonify({
+            'status': 'success',
+            'ml_output': ml_output,
+            'risk_class': "high" if ml_prediction == "bad" else "low",   
+            'llm_output': llm_text
+        })
+
+    except Exception as e:
+        print(f"[ERROR] {e}")  
+
+        return jsonify({
+            'status': 'error',
+            'message': 'Something went wrong on the server'
+        }), 500
+
+
+# -------------------- RUN -------------------- #
 if __name__ == '__main__':
     app.run(debug=True)
